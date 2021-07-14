fc8cb668dd624837a1c1bd1d90622579d24ddb09
diogomcsousa <uceemcs@ucl.ac.uk>
2021-07-15 00:55:49 +0100

    Update dataset

--- main.py
+++ main.py
@@ -12,17 +12,47 @@ import matplotlib.pyplot as plt
 
 def convert():
     df = pd.read_csv('training.csv')
+    df1 = pd.read_csv('diagnosis.csv')
+    df2 = pd.read_csv('dermatology.csv')
 
     new_dataset = pd.DataFrame(columns=['symptoms', 'diagnosis'])
 
     for tup in df.itertuples(index=False):
         symptoms = []
-        diagnosis = tup[len(tup)-1]
+        diagnosis = tup[len(tup)-1].lower()
         for e in range(len(tup)):
             if tup[e] == 1:
-                symptoms.append(df.columns[e].replace('_', ' '))
+                symptoms.append(df.columns[e].replace('_', ' ').lower())
         new_dataset = new_dataset.append({'symptoms': ' '.join(symptoms), 'diagnosis': diagnosis}, ignore_index=True)
 
+    for tup in df1.itertuples(index=False):
+        symptoms = [tup[0].replace(',', '.')]
+        for e in range(1, len(tup) - 2):
+            if tup[e] == "yes":
+                symptoms.append(df1.columns[e].replace(',', '').lower())
+        if tup[len(tup)-1] == "yes":
+            new_dataset = new_dataset.append({'symptoms': ' '.join(symptoms), 'diagnosis': "nephritis"},
+                                             ignore_index=True)
+        if tup[len(tup)-2] == "yes":
+            new_dataset = new_dataset.append({'symptoms': ' '.join(symptoms),
+                                              'diagnosis': "urinary tract infection"},
+                                             ignore_index=True)
+
+    for tup in df2.itertuples(index=False):
+        dermatology = ['', 'psoriasis', 'seboreic dermatitis',
+                       'lichen planus', 'pityriasis rosea',
+                       'cronic dermatitis', 'pityriasis rubra pilaris']
+
+        if tup[len(tup) - 2] != '?':
+            symptoms = [str(tup[len(tup)-2])]
+
+        for e in range(len(tup) - 2):
+            if tup[e] != 0 and tup[e] != '?':
+                symptoms.append(df2.columns[e].replace(',', '').lower())
+        new_dataset = new_dataset.append({'symptoms': ' '.join(symptoms), 'diagnosis': dermatology[tup[len(tup)-1]]},
+                                         ignore_index=True)
+
+    new_dataset.to_csv("medical_diagnosis.csv", index=False)
     corpus = new_dataset['symptoms']
 
     vectorizer = CountVectorizer()
@@ -53,20 +83,12 @@ def convert():
     print(f1_score(Y_test, predicted_categories, average='macro'))
     print(model.classes_)
 
-    test = ['dischromic eruptions itching nodal patches skin']
+    test = ['burning urethra micturition pain 41.0']
     test_ar = vectorizer.transform(test).toarray()
     print(test_ar.shape)
     y_ar = model.predict_proba(test_ar)
     print(model.classes_[np.argmax(y_ar)])
     print(y_ar)
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
