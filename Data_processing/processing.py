733f3bd6a0db1ffc3c386bc8048f9583d93d07fe
diogomcsousa <uceemcs@ucl.ac.uk>
2021-08-11 21:28:01 +0100

    Add disease summary/action

--- Data_processing/processing.py
+++ Data_processing/processing.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+import nltk
+nltk.download("wordnet")
+nltk.download("punkt")
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 import pickle
 
 
@@ -35,7 +42,7 @@ def data_collect():
                                              ignore_index=True)
 
     for tup in df_3.itertuples(index=False):
-        dermatology = ['', 'psoriasis', 'seboreic dermatitis',
+        dermatology = ['', 'psoriasis', 'seborrheic dermatitis',
                        'lichen planus', 'pityriasis rosea',
                        'chronic dermatitis', 'pityriasis rubra pilaris']
 
@@ -49,7 +56,22 @@ def data_collect():
         new_dataset = new_dataset.append({'symptoms': ' '.join(symptoms), 'diagnosis': dermatology[tup[len(tup) - 1]]},
                                          ignore_index=True)
 
-    new_dataset.to_csv("Data/medical_diagnosis.csv", index=False)
+    final_dataset = pd.DataFrame(columns=['symptoms', 'diagnosis'])
+    for tup in new_dataset.itertuples():
+        stop_words = set(stopwords.words('english'))
+        lemmatizer = WordNetLemmatizer()
+        symptoms = []
+        for word, tag in pos_tag(word_tokenize(tup[1])):
+            if word not in stop_words:
+                wntag = tag[0].lower()
+                wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+                if not wntag:
+                    symptoms.append(word)
+                else:
+                    symptoms.append(lemmatizer.lemmatize(word, wntag))
+        final_dataset = final_dataset.append({'symptoms': ' '.join(symptoms), 'diagnosis': tup[2]},
+                                         ignore_index=True)
+    final_dataset.to_csv("Data/medical_diagnosis.csv", index=False)
 
 
 def data_transform():
